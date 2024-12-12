c8423d08dfbd86801b11d70b903cd21d9dde75fb
Paul Hoffer <phoffer@eriestrayer.com>
2024-12-12 17:12:05 -0500

    Day 5 part 2

--- Day5/main.cpp
+++ Day5/main.cpp
@@ -21,12 +21,14 @@ int main()
 	int leftRule = 0;
 	int rightRule = 0;
 	bool doneWithSection1 = false;
+	bool swapped = false;
 
 	//rules and pages
 	std::multimap<int, int> ruleList;
 	std::vector<std::vector<int>> pagesList;
 	bool goodPageList = true;
 	std::vector<std::vector<int>> orderedPagesList;
+	std::vector<std::vector<int>> swappedPagesList;
 	day5File.open("day5Input");
 
 	if(day5File.is_open())
@@ -72,38 +74,64 @@ int main()
 	//loop through all pages
 	for(unsigned int i = 0; i < pagesList.size(); i++)
 	{
-		goodPageList = true;
+		swapped = false;
 		//for each page in the list of pageLists
-		for(auto& page : pagesList[i])
+		for(unsigned int page = 0; page < pagesList[i].size(); page++)
 		{
 			//page from page list
 			//double check multimap
 			auto ruleIterator = ruleList.begin();
 			while(ruleIterator != ruleList.end())
 			{
-				if(ruleIterator->first == page)
+				if(!goodPageList)
+					goodPageList = true;
+				if(ruleIterator->first == pagesList[i][page])
 				{
-					auto xSidePageIterator = find(pagesList[i].begin(), pagesList[i].end(), page);
+					auto xSidePageIterator = find(pagesList[i].begin(), pagesList[i].end(), pagesList[i][page]);
 					auto ySidePageIterator = find(pagesList[i].begin(), pagesList[i].end(), ruleIterator->second);
+					//std::cout << "Got rule " << *xSidePageIterator << " and " << *ySidePageIterator << std::endl;
 					if(xSidePageIterator > ySidePageIterator)
+					{
 						goodPageList = false;
+						swapped = true;
+						int tempX = 0;
+						int tempY = 0;
+						//std::cout << "Swapping " << *xSidePageIterator << " and " << *ySidePageIterator << std::endl;
+						tempX = *xSidePageIterator;
+						tempY = *ySidePageIterator;
+						*ySidePageIterator = tempX;
+						*xSidePageIterator = tempY;
+						//std::cout << "After swapping " << *xSidePageIterator << " and " << *ySidePageIterator << std::endl;
+						//try again!
+						ruleIterator = ruleList.begin();
+						page = 0;
+					}
+					//swap values from xSideIterator and ySideIterator and reset iterator and check entire list again
 				}
 				ruleIterator++;
+				
 			}
 		}
-		if(goodPageList)
+		if(goodPageList && !swapped)
 		{
 			orderedPagesList.push_back(pagesList[i]);
 			std::cout << "Good pages list at i: " << i << std::endl;
 			for(unsigned int j = 0; j < pagesList[i].size(); j++)
-			{
 				std::cout << pagesList[i][j] << " ";
-			}
+			std::cout << std::endl << std::endl;
+		}
+		if(goodPageList && swapped)
+		{
+			swappedPagesList.push_back(pagesList[i]);
+			std::cout << "Swapped pages list at i: " << i << std::endl;
+			for(unsigned int j = 0; j < pagesList[i].size(); j++)
+				std::cout << pagesList[i][j] << " ";
 			std::cout << std::endl << std::endl;
 		}
 	}
 
 	int totalPageListMiddle = 0;
+	int swappedPageListMiddle = 0;
 	for(auto& orderedList : orderedPagesList)
 	{
 		// std::cout << "Ordered Pages: ";
@@ -113,7 +141,13 @@ int main()
 		auto midPgNum = orderedList.begin() + orderedList.size() / 2;
 		totalPageListMiddle += *midPgNum;
 	}
+	for(auto& swappedList : swappedPagesList)
+	{
+		auto midSwapNum = swappedList.begin() + swappedList.size() / 2;
+		swappedPageListMiddle += *midSwapNum;
+	}
 
 	std::cout << "Total of middle value of ordered pages: " << totalPageListMiddle << std::endl;
+	std::cout << "Total of middle value of swapped pages: " << swappedPageListMiddle << std::endl;
 	return 0;
 }
